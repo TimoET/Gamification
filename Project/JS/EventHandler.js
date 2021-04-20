288721095faeb6b03fc954c1fed8ce693b06e825
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2021-04-20 15:16:54 +0200

    Changes to google map api and started making a grid of squares

--- Project/JS/EventHandler.js
+++ Project/JS/EventHandler.js
@@ -4,34 +4,54 @@ import { earth } from './globe.js';
 // Add event listeners so DOM knows what functions to use when objects/items are interacted with
 window.addEventListener('resize', onWindowResize, false);
 window.addEventListener('click', onWindowClick, false);
+window.addEventListener('click', onDocumentMouseMove, false);
 
-// CREATE raycaster for mouse interaction
-const raycaster = new THREE.Raycaster();
+// Mouse
+let raycaster = new THREE.Raycaster();
+let mouse = new THREE.Vector2();
 
-// CREATE vector2 for mouse and mobile x,y coordinates
-const mouse = new THREE.Vector2();
-const touch = new THREE.Vector2();
+function onDocumentMouseMove( event ) {
+
+    event.preventDefault();
+
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
 
 // Listens for the mouse to intersect object and when clicked returns the data to the inner html
 function onWindowClick(event) {
-    console.log('click');
     event.preventDefault();
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
     raycaster.setFromCamera(mouse, camera);
 
-    let intersects = raycaster.intersectObjects(earth.children, true);
-    console.log(earth.children, true);
+    let intersects = raycaster.intersectObjects(earth.children);
 
-    for (let i = 0; i < intersects.length; i++){
-        document.querySelector("#country-info").innerText = "Country: " + intersects[0].object.userData.country;
-        document.querySelector("#area").innerText = "Area: " + intersects[0].object.userData.area;
-        document.querySelector("#area-sq-meter").innerText = "Area(meter^2): " + intersects[0].object.userData.area_sq_meter;
+    for (var i in earth.children) {
+        if (earth.children[i] instanceof THREE.Mesh) 
+        {
+            intersects.push(raycaster.intersectObject(earth.children[i]));
+            console.log(earth.children[i]);
+        }
     }
-    const item = intersects[0];
-    let point = item.point;
-    let camDistance = camera.position.copy(point).normalize.multiplyScalar(camDistance);
+    
+      if (intersects.length > 0) {
+          console.log(intersects);
+        for (let i = 0; i < intersects.length; i++){
+            document.querySelector("#country-info").innerText = "Country: " + intersects[0].object.userData.country;
+            document.querySelector("#area").innerText = "Area: " + intersects[0].object.userData.area;
+            document.querySelector("#area-sq-meter").innerText = "Area(meter^2): " + intersects[0].object.userData.area_sq_meter;
+        }
+        return intersects;
+      } else {
+        return null;
+      }
+
+    
+
 };
+
+function showData(){
+    
+}
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
--- Project/JS/GoogleMap.js
+++ Project/JS/GoogleMap.js
@@ -1,8 +1,36 @@
+import { data } from './dataImport.js';
+
 function initMap() {
     let location = { lat: -34.397, lng: 150.644 };
     let map = new google.maps.Map(document.getElementById("map"), {
     center: location,
     zoom: 8,
+    gestureHandling: "greedy",
+    mapTypeId: google.maps.MapTypeId.HYBRID,
+    mapTypeControl: false,
+  });
+  MarkerMaker(map);
+  Rectangle(map, 35.000900,-120.0009000, 35.000800,-120.0008000);
+}
+
+function MarkerMaker(theMap){
+  for (let i = 0; i < data.length; i++){
+    var myLatlng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      title:data[i].Area
+  });
+  marker.setMap(theMap);
+  }
+}
+
+function Rectangle(theMap,TopCornerLat,TopCornerLng,BotCornerLat,BotCornerLng){
+  let rectangle = new google.maps.Rectangle({
+    map: theMap,
+    bounds: new google.maps.LatLngBounds(
+      new google.maps.LatLng (TopCornerLat, TopCornerLng),
+      new google.maps.LatLng (BotCornerLat, BotCornerLng)
+    )
   });
 }
 

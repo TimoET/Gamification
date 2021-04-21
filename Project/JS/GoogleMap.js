79244e9186cee846dcf6e9267e259c7ea643152d
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2021-04-21 15:36:57 +0200

    Big progress on grid generation

--- Project/JS/GoogleMap.js
+++ Project/JS/GoogleMap.js
@@ -1,7 +1,13 @@
 import { data } from './dataImport.js';
 
+// Amount of Latitude and longatute between meters
+let LatBetweenMeter = 0.000100;
+let LngBetweenMeter = 0.0001000;
+
+
+
 function initMap() {
-    let location = { lat: -34.397, lng: 150.644 };
+    let location = { lat: 34.879172, lng: -120.142419 };
     let map = new google.maps.Map(document.getElementById("map"), {
     center: location,
     zoom: 8,
@@ -10,7 +16,7 @@ function initMap() {
     mapTypeControl: false,
   });
   MarkerMaker(map);
-  Rectangle(map, 35.000900,-120.0009000, 35.000800,-120.0008000);
+  Grid(map, 3, 5, 35.000900,-120.0009000, 35.000800,-120.0008000);
 }
 
 function MarkerMaker(theMap){
@@ -24,7 +30,7 @@ function MarkerMaker(theMap){
   }
 }
 
-function Rectangle(theMap,TopCornerLat,TopCornerLng,BotCornerLat,BotCornerLng){
+function Rectangle(theMap, TopCornerLat, TopCornerLng, BotCornerLat, BotCornerLng){
   let rectangle = new google.maps.Rectangle({
     map: theMap,
     bounds: new google.maps.LatLngBounds(
@@ -32,6 +38,16 @@ function Rectangle(theMap,TopCornerLat,TopCornerLng,BotCornerLat,BotCornerLng){
       new google.maps.LatLng (BotCornerLat, BotCornerLng)
     )
   });
+}
+
+function Grid(theMap, witdh, height, TCLat, TCLng, BCLat, BCLng){
+
+  for(let i = 0; i < witdh; i++){
+    Rectangle(theMap, TCLat, TCLng + LngBetweenMeter * i, BCLat, BCLng + LngBetweenMeter * i);
+  }
+  for(let i = 0; i < height; i++){
+    Rectangle(theMap, TCLat - LatBetweenMeter * i,TCLng, BCLat - LatBetweenMeter * i, BCLng);
+  }
 }
 
 initMap();

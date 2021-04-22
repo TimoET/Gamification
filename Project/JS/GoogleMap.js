030abb3ee780dcaae6ae8fd72040361144ed9b79
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2021-04-22 15:06:21 +0200

    Grid creation on protection zones added

--- Project/JS/GoogleMap.js
+++ Project/JS/GoogleMap.js
@@ -4,8 +4,6 @@ import { data } from './dataImport.js';
 let LatBetweenMeter = 0.000100;
 let LngBetweenMeter = 0.0001000;
 
-
-
 function initMap() {
     let location = { lat: 34.879172, lng: -120.142419 };
     let map = new google.maps.Map(document.getElementById("map"), {
@@ -16,7 +14,7 @@ function initMap() {
     mapTypeControl: false,
   });
   MarkerMaker(map);
-  Grid(map, 3, 5, 35.000900,-120.0009000, 35.000800,-120.0008000);
+  GenerateAllGrids(map);
 }
 
 function MarkerMaker(theMap){
@@ -30,24 +28,46 @@ function MarkerMaker(theMap){
   }
 }
 
-function Rectangle(theMap, TopCornerLat, TopCornerLng, BotCornerLat, BotCornerLng){
+function Rectangle(theMap, TopCornerLat, TopCornerLng, BotCornerLat, BotCornerLng, randomColor){
   let rectangle = new google.maps.Rectangle({
     map: theMap,
     bounds: new google.maps.LatLngBounds(
       new google.maps.LatLng (TopCornerLat, TopCornerLng),
       new google.maps.LatLng (BotCornerLat, BotCornerLng)
-    )
+    ),
+    fillColor: randomColor,
+    strokeColor: randomColor,
+    strokeWeight: 1,
   });
 }
 
 function Grid(theMap, witdh, height, TCLat, TCLng, BCLat, BCLng){
 
   for(let i = 0; i < witdh; i++){
-    Rectangle(theMap, TCLat, TCLng + LngBetweenMeter * i, BCLat, BCLng + LngBetweenMeter * i);
+    Rectangle(theMap, TCLat, TCLng + LngBetweenMeter * i, BCLat, BCLng + LngBetweenMeter * i, getRandomColor());
   }
-  for(let i = 0; i < height; i++){
-    Rectangle(theMap, TCLat - LatBetweenMeter * i,TCLng, BCLat - LatBetweenMeter * i, BCLng);
+  for(let i = 0; i < witdh; i++){
+    for(let j = 1; j < height; j++){
+      Rectangle(theMap, TCLat - LatBetweenMeter * j,TCLng + LngBetweenMeter * i, BCLat - LatBetweenMeter * j, BCLng + LngBetweenMeter * i, getRandomColor());
+    }
   }
+  
 }
 
+function GenerateAllGrids(theMap) {
+  // Get the data from the JSON file
+  for (let i = 0; i < data.length; i++){
+    Grid(theMap, 100, 100, data[i].latitude, data[i].longitude, data[i].latitude + LatBetweenMeter, data[i].longitude + LngBetweenMeter);
+  }
+};
+
 initMap();
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
